e1a3fbced47afc737a2e5e3d5bfacb6ba15f7d5c
흐르미 <boyumi98@gmail.com>
2020-09-04 08:34:18 +0900

    TypeScript + React demo 제작

---
+++ typescript-react-demo/README.md
@@ -0,0 +1,5 @@
+# TypeScript + React
+
+## @types/library 
+
+## @types에 없으면 -> tsconfig.json 에서 <"anyImplicitAny": true> 추가
---
+++ typescript-react-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {createGlobalStyle} from "styled-components";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
